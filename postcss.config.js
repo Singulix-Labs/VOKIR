6cf2c433f4c0164099e2b5642fe4c8b954fee387
Morgan Sterling <git.acct01@gmail.com>
2025-02-13 17:10:50 +0800

    style: add PostCSS plugins for nesting and imports

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,7 @@ module.exports = {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      "postcss-nested": {}, // Enable nesting support
+      "postcss-import": {}, // Enable @import support for modular CSS
     },
-  };
+};
