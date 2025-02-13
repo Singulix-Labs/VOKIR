f9a4ee61a496cd76524dda93ea54fbe9afd9cbd4
Morgan Sterling <git.acct01@gmail.com>
2025-02-13 17:13:03 +0800

    feat: add Contact page route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Token } from "./pages/Token";
+import { Contact } from "./pages/Contact"; // Added Contact page
 import { Footer } from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop"; // Added ScrollToTop component
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Ensures the page scrolls to top on route change */}
       <div className="min-h-screen flex flex-col bg-gray-900 text-white">
         <Navbar />
         <div className="flex-grow" role="main">
@@ -16,6 +19,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/token" element={<Token />} />
+            <Route path="/contact" element={<Contact />} /> {/* New Contact Route */}
           </Routes>
         </div>
         <Footer />
