19f6837391f5a3062ecf3f2fb41cb88522e07a8f
Morgan Sterling <git.acct01@gmail.com>
2025-02-14 12:04:04 +0800

    chore: add postcss-flexbugs-fixes and postcss-preset-env for better CSS support

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,7 @@ module.exports = {
       autoprefixer: {},
       "postcss-nested": {}, // Enable nesting support
       "postcss-import": {}, // Enable @import support for modular CSS
+      "postcss-flexbugs-fixes": {}, // Fixes flexbox issues in older browsers
+      "postcss-preset-env": { stage: 1 } // Adds modern CSS features with fallback support
     },
 };
