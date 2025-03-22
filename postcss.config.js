fdd76797e58d0d0ac5d9172263ea9cb3b504b7ae
Morgan Sterling <git.acct01@gmail.com>
2025-03-22 10:30:00 +0800

    feat(deps): add postcss-flexbugs-fixes and postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,7 @@ module.exports = {
     autoprefixer: {},
     "postcss-nested": {}, // Enables nesting support
     "postcss-import": {}, // Enables @import support for modular CSS
+    "postcss-flexbugs-fixes": {}, // Fixes flexbox issues in older browsers
+    "postcss-preset-env": { stage: 1 }, // Adds modern CSS features with fallback support
   },
 };
