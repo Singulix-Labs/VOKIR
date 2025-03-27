13a165fc097b707b7125c959652062c6618ca4fc
Morgan Sterling <git.acct01@gmail.com>
2025-03-27 15:00:00 +0800

    feat(deps): add postcss-advanced-variables for complex variable support

--- postcss.config.js
+++ postcss.config.js
@@ -11,5 +11,6 @@ module.exports = {
     "postcss-logical": {}, // Enables logical CSS properties for better RTL support
     "postcss-color-mod-function": {}, // Enables advanced color manipulation in CSS
     "postcss-sort-media-queries": {}, // Optimizes media query ordering for performance
+    "postcss-advanced-variables": {}, // Adds support for more complex CSS variables
   },
 };
